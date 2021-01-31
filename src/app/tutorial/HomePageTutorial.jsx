15e31b5f37ae30276e9ba51781e20faccd7275ea
Kacy Sommers <kacy@dev.adaptiva.com>
2021-01-30 18:28:30 -0800

    removing framer adding framer motion

--- src/app/tutorial/HomePageTutorial.jsx
+++ src/app/tutorial/HomePageTutorial.jsx
@@ -1,62 +1,91 @@
-import React from 'react';
-import './tutorial.css';
-import { Messages } from './tutorials';
+import React, { useState, useEffect } from 'react';
+import './tutorials.css';
+import { Slides } from './tutorials';
 import speechBubble from '../../assets/images/speech-bubble.svg';
+import { motion, AnimatePresence } from 'framer-motion';
 
-class HomePageTutorial extends React.Component {
-  timer = 0;
+let timer = 0;
 
-  constructor(props) {
-    super(props);
+const speechBubbleVariants = {
+  enter: { opacity: 0.5, scale: 0.3 },
+  center: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 1.5 },
+};
 
-    this.state = {
-      messageIndex: 0,
-    };
+const HomePageTutorial = () => {
+  const [msgIndex, setMsgIndex] = useState(0);
+
+  if (timer) {
+    window.clearTimeout(timer);
   }
 
-  setMessageIndex(dir) {
-    // dir will be 1 or -1
-
-    console.log('SET MESG');
-
-    if (this.timer) {
-      window.clearTimeout(this.timer);
-      this.timer = 0;
-    }
-
-    this.setState((state) => ({
-      messageIndex: state.messageIndex + dir,
-    }));
+  const currentMsg = Slides.home[msgIndex];
+  if (!currentMsg) {
+    return null;
   }
 
-  render() {
-    const currentMsg = Messages.home[this.state.messageIndex];
-    if (!currentMsg) {
-      console.error('NO MESSAGE!!!');
-      return null;
-    }
+  if (currentMsg.timer) {
+    timer = window.setTimeout(() => {
+      setMsgIndex(msgIndex + 1);
+    }, currentMsg.timer);
+  }
 
-    if (currentMsg.timer) {
-      this.timer = window.setTimeout(
-        this.setMessageIndex.bind(this, 1),
-        currentMsg.timer
-      );
-    }
+  const sharkieVariants = currentMsg.image.variants;
 
-    return (
-      <div className='tutorial-container'>
-        <div className='cartoon-wrap'>
-          <img
-            className='speech-bubble-img'
-            src={speechBubble}
-            alt={currentMsg.message}
-          />
-          {currentMsg.jsx}
-          {currentMsg.image}
-        </div>
+  return (
+    <div className='tutorial-container'>
+      <div className='cartoon-wrap'>
+        <AnimatePresence initial={true}>
+          <motion.div
+            key={msgIndex}
+            className='speech-bubble-wrap'
+            initial='enter'
+            animate='center'
+            exit='exit'
+            transition={{
+              scale: {
+                type: 'spring',
+                stiffness: 500,
+                damping: 30,
+                duration: 0.2,
+              },
+            }}
+            variants={speechBubbleVariants}
+          >
+            <img
+              className='speech-bubble-img'
+              src={speechBubble}
+              alt={currentMsg.message.text}
+            />
+            {currentMsg.message.jsx}
+          </motion.div>
+        </AnimatePresence>
+        <AnimatePresence initial={true}>
+          <motion.div
+            key={currentMsg.image.key}
+            className='sharkie-img-wrap'
+            initial='enter'
+            animate='center'
+            exit='exit'
+            variants={sharkieVariants}
+            transition={{
+              scale: {
+                type: 'spring',
+                stiffness: 500,
+                damping: 30,
+                duration: 0.2,
+              },
+              opacity: {
+                duration: 0.2,
+              },
+            }}
+          >
+            {currentMsg.image.jsx}
+          </motion.div>
+        </AnimatePresence>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default HomePageTutorial;
--- src/app/tutorial/tutorials.jsx
+++ src/app/tutorial/tutorials.jsx
@@ -5,72 +5,158 @@ const messages = [
   'Hello there!',
   'Are you here to deliver my fish?',
   'No?',
-  'Wait, aren`\t you the new Junior Manager of the San Jose Sharks?',
+  "Wait, aren't you the new Junior Manager of the San Jose Sharks?",
+  'Welcome to our stadium!',
+  "My name is Sharkie, I'm here to teach about your new job!",
+  'Ready?',
 ];
 
-export const Messages = {
+export const Slides = {
   home: [
     {
-      message: messages[0],
-      jsx: <p className='tutorial-msg color-primary font-lg'>{messages[0]}</p>,
-      timer: 3000,
+      message: {
+        text: messages[0],
+        jsx: (
+          <p className='tutorial-msg color-primary font-lg'>{messages[0]}</p>
+        ),
+      },
+      image: {
+        key: 'sharkie-play',
+        jsx: (
+          <img className='sharkie-img' src={sharkiePlay} alt={messages[0]} />
+        ),
+        variants: {
+          enter: { opacity: 0.5, scale: 0.3, x: '95%', y: '13%' },
+          center: { opacity: 1, scale: 1, x: '95%', y: '13%' },
+        },
+      },
       buttons: true,
-      image: (
-        <img
-          className='sharkie-img'
-          src={sharkiePlay}
-          style={{ transform: 'translate(92%, 43%)' }}
-          alt={messages[0]}
-        />
-      ),
+      timer: 3000,
     },
     {
-      message: messages[1],
-      jsx: <p className='tutorial-msg color-primary font-lg'>{messages[1]}</p>,
+      message: {
+        text: messages[1],
+        jsx: (
+          <p className='tutorial-msg color-primary font-lg'>{messages[1]}</p>
+        ),
+      },
+      image: {
+        key: 'sharkie-play',
+        jsx: (
+          <img className='sharkie-img' src={sharkiePlay} alt={messages[1]} />
+        ),
+        variants: {
+          enter: { opacity: 0.5, scale: 1, x: '95%', y: '13%' },
+          center: { opacity: 1, scale: 1, x: '95%', y: '13%' },
+        },
+      },
       timer: 3000,
       buttons: true,
-      image: (
-        <img
-          className='sharkie-img'
-          src={sharkiePlay}
-          style={{ transform: 'translate(92%, 43%)' }}
-          alt={messages[1]}
-        />
-      ),
     },
     {
-      message: messages[2],
-      jsx: <p className='tutorial-msg color-primary font-lg'>No?</p>,
+      message: {
+        text: messages[2],
+        jsx: (
+          <p className='tutorial-msg color-primary font-lg'>{messages[2]}</p>
+        ),
+      },
+      image: {
+        key: 'sharkie-play',
+        jsx: (
+          <img className='sharkie-img' src={sharkiePlay} alt={messages[2]} />
+        ),
+        variants: {
+          enter: { opacity: 0.5, scale: 1, x: '95%', y: '13%' },
+          center: { opacity: 1, scale: 1, x: '95%', y: '13%' },
+          exit: { opacity: 0 },
+        },
+      },
       timer: 3000,
       buttons: true,
-      image: (
-        <img
-          className='sharkie-img'
-          src={sharkieSpeak}
-          style={{ transform: 'translate(85%, 50%)' }}
-          alt={messages[2]}
-        />
-      ),
     },
     {
-      message: messages[3],
-      jsx: (
-        <p className='tutorial-msg color-primary font-lg'>
-          Wait, aren't you the new{' '}
-          <span className='color-accent font-xl'>Junior Manager</span> of the
-          San Jose Sharks?
-        </p>
-      ),
+      message: {
+        text: messages[3],
+        jsx: (
+          <p className='tutorial-msg color-primary font-lg'>
+            Wait, aren't you the new{' '}
+            <span className='color-accent font-xl'>Junior Manager</span> of the
+            San Jose Sharks?
+          </p>
+        ),
+      },
+      image: {
+        key: 'sharkie-speak',
+        jsx: (
+          <img className='sharkie-img' src={sharkieSpeak} alt={messages[3]} />
+        ),
+        variants: {
+          enter: { opacity: 0, scale: 1, x: '95%', y: '13%' },
+          center: { opacity: 1, scale: 1, x: '95%', y: '15%' },
+        },
+      },
+      timer: 3000,
+      buttons: true,
+    },
+    {
+      message: {
+        text: messages[4],
+        jsx: (
+          <p className='tutorial-msg color-primary font-lg'>{messages[4]}</p>
+        ),
+      },
+      image: {
+        key: 'sharkie-speak',
+        jsx: (
+          <img className='sharkie-img' src={sharkieSpeak} alt={messages[4]} />
+        ),
+        variants: {
+          enter: { opacity: 1, scale: 1, x: '95%', y: '15%' },
+          center: { opacity: 1, scale: 1, x: '95%', y: '15%' },
+        },
+      },
+      timer: 3000,
+      buttons: true,
+    },
+    {
+      message: {
+        text: messages[5],
+        jsx: (
+          <p className='tutorial-msg color-primary font-lg'>{messages[5]}</p>
+        ),
+      },
+      image: {
+        key: 'sharkie-speak',
+        jsx: (
+          <img className='sharkie-img' src={sharkieSpeak} alt={messages[5]} />
+        ),
+        variants: {
+          enter: { opacity: 1, scale: 1, x: '95%', y: '15%' },
+          center: { opacity: 1, scale: 1, x: '95%', y: '15%' },
+        },
+      },
+      timer: 3000,
+      buttons: true,
+    },
+    {
+      message: {
+        text: messages[6],
+        jsx: (
+          <p className='tutorial-msg color-primary font-lg'>{messages[6]}</p>
+        ),
+      },
+      image: {
+        key: 'sharkie-speak',
+        jsx: (
+          <img className='sharkie-img' src={sharkieSpeak} alt={messages[6]} />
+        ),
+        variants: {
+          enter: { opacity: 1, scale: 1, x: '95%', y: '15%' },
+          center: { opacity: 1, scale: 1, x: '95%', y: '15%' },
+        },
+      },
       timer: 0,
       buttons: true,
-      image: (
-        <img
-          className='sharkie-img'
-          src={sharkieSpeak}
-          style={{ transform: 'translate(85%, 50%)' }}
-          alt={messages[2]}
-        />
-      ),
     },
   ],
 };
