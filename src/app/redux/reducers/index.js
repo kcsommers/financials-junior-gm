0091dd3250306d9f4996b3d9cf9ed7db6203b456
Kacy Sommers <kacy@dev.adaptiva.com>
2021-02-05 23:13:31 -0800

    adding modal component

--- src/app/redux/reducers/index.js
+++ src/app/redux/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import tutorials from './tutorials';
+import modal from './modal';
 
-export default combineReducers({ tutorials });
+export default combineReducers({ tutorials, modal });
