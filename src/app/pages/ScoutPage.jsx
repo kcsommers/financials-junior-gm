e493bcc53b3a1a6087351913b9c7e75acfc17603
Kacy Sommers <kacy@dev.adaptiva.com>
2021-02-06 15:46:03 -0800

    adding money level component

--- src/app/pages/ScoutPage.jsx
+++ src/app/pages/ScoutPage.jsx
@@ -10,6 +10,7 @@ import {
   SignStick,
   ScoutStick,
   PlayerCard,
+  MoneyLevel,
 } from '@components';
 import sharksLogo from '@images/sharks-comerica-logo.svg';
 import { useSelector, useDispatch } from 'react-redux';
@@ -19,8 +20,6 @@ import { Link } from 'react-router-dom';
 import { setTutorialIsActive } from '@redux/actions';
 import '@css/pages/page.css';
 import '@css/pages/ScoutPage.css';
-
-import hmm from './hmm.svg';
 
 const teamSlides = [playersSlides];
 
@@ -50,7 +49,34 @@ function TeamPage() {
           ></ObjectivesBoard>
         </div>
       </div>
-      <div className='page-body'></div>
+      <div className='page-body'>
+        <div className='page-board scout-page-board'>
+          <div className='scout-page-board-header'>
+            <p className='color-primary'>
+              Give each new player a contract value by dragging them to their
+              money level!
+            </p>
+          </div>
+          <div className='scout-page-board-inner'>
+            <div className='scout-page-board-left'>
+              <div className='card auto-card'></div>
+            </div>
+            <div className='scout-page-board-right'>
+              <div className='card auto-card'>
+                <MoneyLevel level='twoDollars' />
+                <MoneyLevel level='oneDollar' />
+                <MoneyLevel level='fiftyCents' />
+              </div>
+            </div>
+          </div>
+          <div className='scout-page-board-footer'>
+            <p className='color-primary'>
+              Remember to tap a player to learn more about them!
+            </p>
+            <span className='color-primary'>Click here when you finish!</span>
+          </div>
+        </div>
+      </div>
       <AnimatePresence>
         {tutorialActive && (
           <motion.div animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
