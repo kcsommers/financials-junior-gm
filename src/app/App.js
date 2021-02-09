a23722a4748ce0c5a0b972af43bf0b097f511adc
Jacob Spade <spade.jacob@Yahoo.com>
2021-02-08 22:01:04 -0800

    first sign page/component

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Intro from './pages/Intro';
 import HomePage from './pages/HomePage';
 import TeamPage from './pages/TeamPage';
-import Scout from './pages/Scout';
 import Season from './pages/Season';
-import Scout from './pages/ScoutPage';
+import ScoutPage from './pages/ScoutPage';
 import '@css/App.css';
 import { Overlay } from '@components';
 
@@ -17,7 +16,7 @@ const App = () => {
           <Route exact path='/' component={Intro} />
           <Route exact path='/home' component={HomePage} />
           <Route exact path='/team' component={TeamPage} />
-          <Route exact path='/scout' component={Scout} />
+          <Route exact path='/scout' component={ScoutPage} />
           <Route exact path='/season' component={Season} />
         </Switch>
       </Router>
--- src/app/components/SignPlayer.jsx
+++ src/app/components/SignPlayer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {TeamRankStick}  from './TeamRankStick';
 import {MoneyLeftStick}  from './MoneyLeftStick';
 import { ReactSVG } from 'react-svg';
-import playerImage from '@images/icons/player-image.svg';
+import playerImageBig from '@images/icons/player-image-big.svg';
 import cancelBig from '@images/icons/cancel-big.svg';
 import confirmBig from '@images/icons/confirm-big.svg'
 import '../../assets/css/components/sign-player.css';
@@ -10,11 +10,16 @@ import '../../assets/css/components/sign-player.css';
 export const SignPlayer = () => {
   return (
     <div className="sign-player-dashboard">
-      <p>Changes To Rank And Budget</p>
-      <TeamRankStick/>
-      <MoneyLeftStick/>
-      <div>
-        <p className='player-card-title'>Position</p>
+      <div className="sign-player-dashboard-top-row">
+        <div>
+          <p className="changes-to-rank-budget">Changes To Rank And Budget</p>
+          <div className="sign-player-visual-state">
+            <TeamRankStick/>
+            <MoneyLeftStick/>
+          </div>
+        </div>
+        <div>
+          <p className='player-card-title'>Position</p>
           <div className='team-dashboard-players-card-top'>
             <div>
               <div className='rank-text'>Rank</div>
@@ -23,7 +28,8 @@ export const SignPlayer = () => {
             <div className='player-cost'>$2</div>
           </div>
           <div className='team-dashboard-players-card'>
-            <ReactSVG src={playerImage} />
+            <p>Name</p>
+            <ReactSVG src={playerImageBig} />
             <div className='player-meters-row'>
               <div>
                 <p>Off</p>
@@ -40,11 +46,24 @@ export const SignPlayer = () => {
             </div>
           </div>
         </div>
-        <p>Are you sure you want to sign the following player?</p>
-        <p>Cancel</p>
-        <ReactSVG src={cancelBig}/>
-        <p>Confirm</p>
-        <ReactSVG src={confirmBig}/>
+
+      </div>
+      
+      
+        
+        <p className="sign-player-question">Are you sure you want to sign the following player?</p>
+        <div className="sign-player-options">
+          <div>
+            <p>Cancel</p>
+            <ReactSVG src={cancelBig}/>
+          </div>
+          <div>
+            <p>Confirm</p>
+            <ReactSVG src={confirmBig}/>
+          </div>
+        </div>
+        
+        
     </div>
   )
 }
