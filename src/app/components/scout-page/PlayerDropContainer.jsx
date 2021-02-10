edb22557321b69426365218cd9e7bb6338952f5a
Kacy Sommers <kacy@dev.adaptiva.com>
2021-02-09 19:05:01 -0800

    redoing scout page

--- src/app/components/scout-page/PlayerDropContainer.jsx
+++ src/app/components/scout-page/PlayerDropContainer.jsx
@@ -1,5 +1,5 @@
-export const DropContainer = (props) => {
-  const { provided, innerRef, children } = props;
+export const PlayerDropContainer = (props) => {
+  const { provided, innerRef, children, player } = props;
   return (
     <div {...provided.droppableProps} ref={innerRef} className='drop-container'>
       {children}
