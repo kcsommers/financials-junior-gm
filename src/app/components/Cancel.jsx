5a9a63f0dc38e536c2bcff7bfc21cd43b79acb2a
Jacob Spade <spade.jacob@Yahoo.com>
2021-02-11 08:37:29 -0800

    completed trade player overlays

--- src/app/components/Cancel.jsx
+++ src/app/components/Cancel.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import {ReactSVG} from 'react-svg'
+// import React from 'react'
+// import {ReactSVG} from 'react-svg'
 
-export const Cancel = () => {
-  return (
-    <div>
-      <p>Cancel</p>
-      <ReactSVG style={{cursor: 'pointer'}} onClick={handleCancel} src={cancelBig}/>
-    </div>
-  )
-}
+// export const Cancel = () => {
+//   return (
+//     <div>
+//       <p>Cancel</p>
+//       <ReactSVG style={{cursor: 'pointer'}} onClick={handleCancel} src={cancelBig}/>
+//     </div>
+//   )
+// }
