d7698f80eec6fcd7cf799a1e028aa3c20f050dcd
Kacy Sommers <kacy@dev.adaptiva.com>
2021-02-11 09:06:29 -0800

    adding keys to scout page loops

--- src/app/components/scout-page/PlayerDropContainer.jsx
+++ src/app/components/scout-page/PlayerDropContainer.jsx
@@ -1,7 +1,13 @@
 export const PlayerDropContainer = (props) => {
   const { provided, innerRef, children, player } = props;
   return (
-    <div {...provided.droppableProps} ref={innerRef} className='drop-container'>
+    <div
+      {...provided.droppableProps}
+      ref={innerRef}
+      className={`drop-container${
+        player ? ' drop-disabled' : ' drag-disabled'
+      }`}
+    >
       {children}
     </div>
   );
