34d3640bc6f48612c1bddaf46f331e2bd286f195
Jacob Spade <spade.jacob@Yahoo.com>
2021-02-10 19:18:17 -0800

    adding trade player process

--- src/app/components/SignPlayer.jsx
+++ src/app/components/SignPlayer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {TeamRankStick}  from './TeamRankStick';
 import {MoneyLeftStick}  from './MoneyLeftStick';
 import { ReactSVG } from 'react-svg';
-import playerImageBig from '@images/icons/player-image-big.svg';
 import cancelBig from '@images/icons/cancel-big.svg';
 import confirmBig from '@images/icons/confirm-big.svg'
 import '../../assets/css/components/sign-player.css';
@@ -43,34 +42,7 @@ export const SignPlayer = () => {
             <MoneyLeftStick/>
           </div>
         </div>
-        <div>
-          <p className='player-card-title'>Position</p>
-          <div className='team-dashboard-players-card-top'>
-            <div>
-              <div className='rank-text'>Rank</div>
-              <div className='rank-number'>50</div>
-            </div>
-            <div className='player-cost'>$2</div>
-          </div>
-          <div className='team-dashboard-players-card'>
-            <p>Name</p>
-            <ReactSVG src={playerImageBig} />
-            <div className='player-meters-row'>
-              <div>
-                <p>Off</p>
-                <div className='offense-meter'></div>
-              </div>
-              <div>
-                <p>Pass</p>
-                <div className='passing-meter'></div>
-              </div>
-              <div>
-                <p>Def</p>
-                <div className='defense-meter'></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        
 
       </div>
       
--- src/app/components/trade-overlay/FindTradePlayer.jsx
+++ src/app/components/trade-overlay/FindTradePlayer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../../../assets/css/components/trade-overlay/find-trade-player.css';
+import {FilledPlayerCard} from '../public-api';
+import { SpacesInTeam, PlayersAvailable } from '../public-api';
 
 export const FindTradePlayer = () => {
   return (
     <div className="trade-dashboard">
       <p>Pick a player to trade with NAME</p>
+      <FilledPlayerCard/>
+      <SpacesInTeam/>
+      <PlayersAvailable/>
     </div>
   )
 }
