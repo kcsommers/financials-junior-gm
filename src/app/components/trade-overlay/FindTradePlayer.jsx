21d3d66fc3b4a9fa96b1a3837f5e0f7505b32021
Jacob Spade <spade.jacob@Yahoo.com>
2021-02-10 20:09:46 -0800

    trade process

--- src/app/components/trade-overlay/FindTradePlayer.jsx
+++ src/app/components/trade-overlay/FindTradePlayer.jsx
@@ -6,11 +6,20 @@ import { SpacesInTeam, PlayersAvailable } from '../public-api';
 export const FindTradePlayer = () => {
   return (
     <div className="trade-dashboard">
-      <p>Pick a player to trade with NAME</p>
-      <FilledPlayerCard/>
-      <SpacesInTeam/>
+      <div className="find-trade-top-row">
+        <div className="player-being-traded">
+          <p className="player-to-trade-title">Pick a player to trade with NAME</p>
+          <div style={{ margin: '0 auto'}}>
+            <FilledPlayerCard/>
+          </div>
+          
+        </div>
+        <div>
+          <SpacesInTeam/>
+        </div>
+      </div>
+      
       <PlayersAvailable/>
-      <p>test</p>
     </div>
   )
 }
