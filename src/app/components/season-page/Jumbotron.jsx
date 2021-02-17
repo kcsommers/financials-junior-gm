2dd495397cc5ace7900f94bd274d50ae38c492da
Kacy Sommers <kacy@dev.adaptiva.com>
2021-02-17 11:46:34 -0800

    jumbotron styling

--- src/app/components/season-page/Jumbotron.jsx
+++ src/app/components/season-page/Jumbotron.jsx
@@ -1,150 +1,165 @@
 import { TeamCard } from './TeamCard';
 import jrSharksLogo from '@images/icons/jr-sharks-logo-white-bg.svg';
+import { GamePhases, allTeams } from '@data/season/season';
+import { Indicator } from '@components';
+import '@css/components/season-page/Jumbotron.css';
 
-const styles = {
-  container: {
-    display: 'flex',
-    height: '324.19px',
-    position: 'relative',
-    width: '464px',
-    zIndex: 0,
-  },
-  main: {
-    background: '#707070',
-    textAlign: 'center',
-    height: '100%',
-    flex: 1,
-    zIndex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  borderLeft: {
-    height: '100%',
-    width: '100%',
-    position: 'absolute',
-    top: 0,
-    left: '-35px',
-    backgroundColor: '#ababab',
-    transform: 'skewX(10deg)',
-  },
-  borderRight: {
-    height: '100%',
-    width: '100%',
-    position: 'absolute',
-    top: 0,
-    right: '-35px',
-    backgroundColor: '#ababab',
-    transform: 'skewX(-10deg)',
-  },
-  message: {
-    width: '550px',
-    minHeight: '70px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: '10px',
-    fontSize: '2.25rem',
-    backgroundColor: '#00788a',
-    color: '#fff',
-  },
-  mainTop: {
-    display: 'flex',
-    padding: '1rem',
-    position: 'relative',
-    flex: 1,
-  },
-  mainTopLeft: {
-    flex: 1,
-  },
-  mainTopRight: {
-    flex: 1,
-  },
-  mainBottom: {
-    flex: 1,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '0 3rem',
-  },
-  scoreContainer: {
-    flex: 1,
-    display: 'flex',
-    alignItems: 'center',
-    position: 'relative',
-    justifyContent: 'space-between',
-  },
-  scoreTitle: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    fontSize: '2rem',
-    textAlign: 'center',
-    color: '#121210',
-  },
-  scoreWrap: {
-    width: '125px',
-    height: '100px',
-    borderRadius: '10px',
-    backgroundColor: '#fff',
-    fontSize: '3rem',
-    color: '#121210',
-    textAlign: 'center',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-};
+export const Jumbotron = ({
+  gameBlockState,
+  student,
+  seasonState,
+  currentOpponentIndex,
+}) => {
+  const { currentOppenent, currentScore, currentPhase } = gameBlockState;
 
-export const Jumbotron = ({ gameBlockState, student, seasonState }) => {
-  const { currentOppenent, currentScore } = gameBlockState;
+  const nextOpponent = allTeams[currentOpponentIndex + 1];
+  const upcomingGames = allTeams.slice(
+    currentOpponentIndex + 2,
+    currentOpponentIndex + 4
+  );
 
   const scoreView = (
-    <div style={styles.scoreContainer}>
-      <div style={styles.scoreTitle}>Score</div>
-      <div style={styles.scoreWrap}>{currentScore[0]}</div>
-      <div style={styles.scoreWrap}>{currentScore[1]}</div>
+    <div className='jumbotron-score-container'>
+      <div className='jumbotron-score-title'>Score</div>
+      <div className='box-shadow jumbotron-score-wrap'>{currentScore[0]}</div>
+      <div className='box-shadow jumbotron-score-wrap'>{currentScore[1]}</div>
     </div>
   );
 
-  return (
-    <>
-      <div style={styles.container}>
-        <div style={styles.borderLeft}></div>
-        <div style={styles.main}>
-          <div style={styles.mainTop}>
-            <div style={styles.mainTopLeft}>
-              <TeamCard logo={jrSharksLogo} rank={student.teamRank} />
+  const statsView = (
+    <div className='jumbotron-stats-container'>
+      <div className='jumbotron-stats-title'>Stats</div>
+      <div className='jumbotron-stats-inner'>
+        <div className='jumbotron-stat-wrap'>
+          Wins
+          <div className='box-shadow jumbotron-stat-inner'>
+            {currentScore[0]}
+          </div>
+        </div>
+        <div className='jumbotron-stat-wrap'>
+          Losses
+          <div className='box-shadow jumbotron-stat-inner'>
+            {currentScore[0]}
+          </div>
+        </div>
+        <div className='jumbotron-stat-wrap'>
+          Points
+          <div className='box-shadow jumbotron-stat-inner'>
+            {currentScore[0]}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  const comingUpView = (
+    <div className='jumbotron-coming-up-container'>
+      <div className='jumbotron-next-opponent-container'>
+        <h3>Next Opponent</h3>
+        <div className='jumbotron-next-opponent-card'>
+          <div className='coming-up-opponent-row'>
+            <div className='coming-up-opponent-name-wrap'>
+              <span className='coming-up-opponent-name'>
+                {nextOpponent.name}
+              </span>
             </div>
-            <span
-              style={{
-                fontSize: '1rem',
-                position: 'absolute',
-                left: 0,
-                display: 'block',
-                top: '50%',
-                transform: 'translate(-50% -50%)',
-                textAlign: 'center',
-                width: '100%',
-              }}
-            >
-              VS
-            </span>
-            <div style={styles.mainTopLeftRight}>
-              <TeamCard
-                logo={currentOppenent.logoLg}
-                rank={currentOppenent.rank}
+            <div className='opponent-indicator-wrap'>
+              <Indicator
+                amount={nextOpponent.teamRank}
+                direction='left'
+                color={nextOpponent.color}
               />
             </div>
           </div>
-
-          <div style={styles.mainBottom}>{scoreView}</div>
         </div>
-        <div style={styles.borderRight}></div>
       </div>
-      <h2 className='jumbotron-message' style={styles.message}>
-        {gameBlockState.message}
-      </h2>
+      <div className='jumbotron-upcoming-games-container'>
+        <h4>Upcoming Games</h4>
+        <div className='jumbotron-upcoming-games-card'>
+          {upcomingGames.map((team, i) => (
+            <div key={i} className='upcoming-games-row coming-up-opponent-row'>
+              <div className='coming-up-opponent-name-wrap'>
+                <span className='coming-up-opponent-name'>{team.name}</span>
+              </div>
+              <div className='opponent-indicator-wrap'>
+                <Indicator
+                  amount={team.teamRank}
+                  direction='left'
+                  color={team.color}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+
+  const gameOnView = (
+    <>
+      <div className='game-on-top'>
+        <div className='game-on-top-left'>
+          <TeamCard logo={jrSharksLogo} rank={student.teamRank} />
+        </div>
+        <span
+          style={{
+            fontSize: '1rem',
+            position: 'absolute',
+            left: 0,
+            display: 'block',
+            top: '50%',
+            transform: 'translate(-50% -50%)',
+            textAlign: 'center',
+            width: '100%',
+            color: '#fff',
+          }}
+        >
+          VS
+        </span>
+        <div className='game-on-top-right'>
+          <TeamCard logo={currentOppenent.logoLg} rank={currentOppenent.rank} />
+        </div>
+      </div>
+      <div className='game-on-bottom'>{scoreView}</div>
+    </>
+  );
+
+  const transitionView = (
+    <div className='transition-view-container'>
+      <div className='transition-view-left'>
+        <TeamCard logo={jrSharksLogo} rank={student.teamRank} />
+        {statsView}
+      </div>
+      <div className='transition-view-right'>{comingUpView}</div>
+    </div>
+  );
+
+  const getJumbotronView = () => {
+    switch (currentPhase.phase) {
+      case GamePhases.READY:
+      case GamePhases.TRANSITION: {
+        return transitionView;
+      }
+      case GamePhases.UP_NEXT:
+      case GamePhases.WARMING_UP:
+      case GamePhases.GAME_ON:
+      case GamePhases.GAME_OVER: {
+        return gameOnView;
+      }
+      default: {
+        return null;
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className='jumbotron-wrap'>
+        <div className='jumbotron-border jumbotron-border-left'></div>
+        <div className='jumbotron-main-section'>{getJumbotronView()}</div>
+        <div className='jumbotron-border jumbotron-border-right'></div>
+      </div>
+      <h2 className='jumbotron-message box-shadow'>{gameBlockState.message}</h2>
     </>
   );
 };
