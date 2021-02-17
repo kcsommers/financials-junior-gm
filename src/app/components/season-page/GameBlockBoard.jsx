548cb3993ca546d6460d9b6ed2e15fc656d542cd
Kacy Sommers <kacy@dev.adaptiva.com>
2021-02-17 08:08:36 -0800

    initial season page flow

---
+++ src/app/components/season-page/GameBlockBoard.jsx
@@ -0,0 +1,3 @@
+export const GameBlockBoard = () => {
+  return <div></div>;
+};
---
+++ src/app/pages/SeasonPage.jsx
@@ -0,0 +1,213 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  HeaderComponent,
+  PageBoard,
+  LoadingSpinner,
+  Jumbotron,
+  LevelStick,
+  GameBlockBoard,
+  StandingsBoard,
+  StartGameButton,
+} from '@components';
+import seasonStick from '@images/season-stick.svg';
+import '@css/pages/SeasonPage.css';
+import { GamePhases, gamePhases } from '@data/season/season';
+import { cloneDeep } from 'lodash';
+
+let timer = 0;
+
+const SeasonPage = () => {
+  const student = useSelector((state) => state.studentState.student);
+
+  const seasonState = useSelector((state) => state.season);
+
+  const [state, setState] = useState({
+    currentBlock: seasonState.gameBlocks[seasonState.gameBlockIndex],
+    currentOpponentIndex: 0,
+    currentPhaseIndex: 0,
+    currentMessageIndex: 0,
+  });
+
+  const currentPhase = gamePhases[state.currentPhaseIndex];
+
+  if (timer) {
+    window.clearTimeout(timer);
+  }
+
+  const gameBlockState = {
+    currentOppenent: state.currentBlock[state.currentOpponentIndex],
+    currentPhase: gamePhases[state.currentPhaseIndex],
+    currentScore: [0, 0],
+    message:
+      gamePhases[state.currentPhaseIndex].messages[state.currentMessageIndex],
+  };
+
+  if (gameBlockState.currentPhase.phase === GamePhases.UP_NEXT) {
+    gameBlockState.currentPhase.messages[1] = `Jr Sharks vs ${gameBlockState.currentOppenent.name}`;
+  }
+
+  if (gameBlockState.currentPhase.phase === GamePhases.GAME_OVER) {
+    // GAME PLAY LOGIC HERE
+    gameBlockState.message = gameBlockState.currentPhase.messages[0];
+  }
+
+  console.log('GAME BLOCK STATE:::: ', gameBlockState);
+
+  const endBlock = () => {
+    console.log('END BLOCK::::');
+  };
+
+  const nextGame = () => {
+    const nextOpponentIndex = state.currentOpponentIndex + 1;
+    const clonedState = cloneDeep(state);
+    clonedState.currentOpponentIndex = nextOpponentIndex;
+    clonedState.currentPhaseIndex = 1; // skip the first phase
+    clonedState.currentMessageIndex = 0;
+    setState(clonedState);
+  };
+
+  const nextPhase = () => {
+    // end of games, throw scenario and set redux state
+    if (
+      state.currentOpponentIndex === state.currentBlock.length - 1 &&
+      gameBlockState.currentPhase.phase === GamePhases.GAME_OVER
+    ) {
+      endBlock();
+      return;
+    }
+    // end of phases, go to next game
+    if (gameBlockState.currentPhase.phase === GamePhases.TRANSITION) {
+      nextGame();
+      return;
+    }
+
+    const nextPhaseIndex = state.currentPhaseIndex + 1;
+    const clonedState = cloneDeep(state);
+    clonedState.currentPhaseIndex = nextPhaseIndex;
+    clonedState.currentMessageIndex = 0;
+    setState(clonedState);
+  };
+
+  const nextMessage = () => {
+    // end of messages, go to next phase
+    if (state.currentMessageIndex === currentPhase.messages.length - 1) {
+      nextPhase();
+      return;
+    }
+    const nextMessageIndex = state.currentMessageIndex + 1;
+    console.log('NEXT MESSAGE:::: ', nextMessageIndex);
+    const clonedState = cloneDeep(state);
+    clonedState.currentMessageIndex = nextMessageIndex;
+    setState(clonedState);
+  };
+
+  const startGameBlock = () => {
+    setState({
+      ...state,
+      currentPhaseIndex: 1,
+    });
+  };
+
+  if (currentPhase.messages.length > 1 && currentPhase.messageTimer) {
+    timer = window.setTimeout(nextMessage, currentPhase.messageTimer);
+  } else if (currentPhase.timer) {
+    timer = window.setTimeout(nextPhase, currentPhase.timer);
+  }
+
+  return student ? (
+    <div className='page-container'>
+      <HeaderComponent
+        stickBtn={seasonStick}
+        objectives={['1. Play the season']}
+        level={student.level}
+      />
+
+      <PageBoard hideCloseBtn={true} includeBackButton={true}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+          }}
+        >
+          <div className='season-page-board-top'>
+            <div className='student-team-rank-container'>
+              <LevelStick
+                type='teamRank'
+                amount={student.teamRank}
+                denom={100}
+                color='#e06d00'
+                indicatorDirection='right'
+                isLarge={true}
+                textJsx={
+                  <span>
+                    Team <br />
+                    Rank
+                  </span>
+                }
+              />
+            </div>
+            <div className='jumbotron-container'>
+              <Jumbotron
+                gameBlockState={gameBlockState}
+                student={student}
+                seasonState={seasonState}
+              />
+            </div>
+            <div className='opposing-team-rank-container'>
+              <LevelStick
+                type='teamRank'
+                amount={student.teamRank}
+                denom={100}
+                color='#e06d00'
+                indicatorDirection='left'
+                isLarge={true}
+                inverse={true}
+                textJsx={
+                  <span>
+                    Team <br />
+                    Rank
+                  </span>
+                }
+              />
+            </div>
+          </div>
+          <div className='season-page-board-bottom'>
+            <div className='play-btn-container'>
+              <span className='play-btn-wrap'>
+                <StartGameButton
+                  onClick={startGameBlock}
+                  gameBlockState={gameBlockState}
+                />
+              </span>
+            </div>
+            <div className='game-block-board-container'>
+              <GameBlockBoard />
+            </div>
+            <div className='standings-board-container'>
+              <StandingsBoard />
+            </div>
+          </div>
+        </div>
+      </PageBoard>
+    </div>
+  ) : (
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <LoadingSpinner />
+    </div>
+  );
+};
+
+export default SeasonPage;
