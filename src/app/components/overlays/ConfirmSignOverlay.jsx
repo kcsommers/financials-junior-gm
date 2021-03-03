bd27856594bf13d2c0aff8d792a69c1cb2126867
Kacy Sommers <kacy@dev.adaptiva.com>
2021-03-03 13:39:20 -0800

    disabling things while tutorial is running

--- src/app/components/overlays/ConfirmSignOverlay.jsx
+++ src/app/components/overlays/ConfirmSignOverlay.jsx
@@ -1,12 +1,13 @@
 import { PlayerCard, ConfirmOverlay, TeamBudgetState } from '@components';
 import '@css/components/team-page/SignPlayerOverlay.css';
 
-export const ConfirmSignOverlay = ({ cancel, confirm, player }) => {
+export const ConfirmSignOverlay = ({ cancel, confirm, player, isDisabled }) => {
   return (
     <ConfirmOverlay
       message='Are you sure you want to sign this player?'
       cancel={cancel}
       confirm={confirm}
+      isDisabled={isDisabled}
     >
       <div style={{ display: 'flex', padding: '2rem 3rem 0 3rem' }}>
         <div style={{ flex: 1 }}>
