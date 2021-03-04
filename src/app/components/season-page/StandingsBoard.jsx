82637eab527445dcee0a0be8acc77d8e454d9726
Kacy Sommers <kacy@dev.adaptiva.com>
2021-03-03 20:20:18 -0800

    ensuring student team is always visible in standings

--- src/app/components/season-page/StandingsBoard.jsx
+++ src/app/components/season-page/StandingsBoard.jsx
@@ -1,16 +1,28 @@
 import { useSelector } from 'react-redux';
+import { cloneDeep } from 'lodash';
 
 export const StandingsBoard = () => {
   const { standings, seasonTeam } = useSelector((state) => state.season);
 
-  const rows = standings.slice(0, 5).map((team, i) => (
+  const clonedStandings = cloneDeep(standings);
+  const studentTeamIndex = clonedStandings.findIndex(
+    (t) => t.name === seasonTeam.name
+  );
+
+  // make sure student team is always visible on the board
+  if (studentTeamIndex > 4) {
+    clonedStandings.splice(4, 0, seasonTeam);
+  }
+
+  const rows = clonedStandings.slice(0, 5).map((team, i) => (
     <div key={i} className='standings-board-row'>
       <span
         className={`${
           team.name === seasonTeam.name ? 'color-primary font-bold' : ''
         }`}
       >
-        {i + 1}. {team.name}
+        {team.name === seasonTeam.name ? studentTeamIndex + 1 : i + 1}.{' '}
+        {team.name}
       </span>
       <span
         className={`${
