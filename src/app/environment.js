e0d819a3ce7a71f14225e2740408120157b3f517
Nandhakumar <rsnk2013@gmail.com>
2021-05-04 20:27:33 +0530

    Updated Code Base

--- src/app/environment.js
+++ src/app/environment.js
@@ -3,7 +3,7 @@ export const environments = {
     HOST_NAME: 'https://www.finjuniorgm.com',
     API_BASE_URL: 'https://www.finjuniorgmapi.com', //'http://ec2-18-236-146-143.us-west-2.compute.amazonaws.com'; //AWS PROD
   },
-  development: {
+  development: { 
     HOST_NAME: 'http://localhost:3000',
     API_BASE_URL: 'https://www.finjuniorgmapi.com', //'http://ec2-18-236-146-143.us-west-2.compute.amazonaws.com'; //AWS TEST
   },
