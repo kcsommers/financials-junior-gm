f566abcdf5601e153147fc0a347fe35c43aa53e4
Kacy Sommers <kacysommers@gmail.com>
2021-08-28 10:11:41 -0700

    rename to ResetPasswordPage

--- src/app/pages/login/ResetPasswordPage.jsx
+++ src/app/pages/login/ResetPasswordPage.jsx
@@ -1,11 +1,9 @@
 import financialsLogo from '@images/financials-logo-big.svg';
-import { LoginForm } from '@components';
-import { UserRoles } from '@data/auth/auth';
 import '@css/pages/Login.css';
 
-export const ForgotPasswordPage = ({ history }) => {
+export const ResetPasswordPage = ({ history }) => {
   return (
-    <div className="login-page-container">
+    <div className="reset-password-page-container">
       <div>
         <img src={financialsLogo} alt="Financials Junior GM Program logo" />
       </div>
