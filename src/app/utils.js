c32d76f6e724aff3e71d05681521550636fffbdd
Kacy Sommers <kacysommers@gmail.com>
2021-12-20 12:40:58 -0800

    add commafyNumber utility function

--- src/app/utils.js
+++ src/app/utils.js
@@ -2,18 +2,40 @@ export const capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
+export const commafyNumber = (num) => {
+  if (!num) {
+    return num;
+  }
+  let counter = 0;
+  let intString = typeof num === 'number' ? num.toString() : num;
+  let decString = '';
+  if (intString.includes('.')) {
+    const decIndex = intString.indexOf('.');
+    decString = intString.substring(decIndex);
+    intString = intString.substring(0, decIndex);
+  }
+  for (let i = intString.length; i > 0; i--) {
+    if (counter === 3) {
+      intString = intString.substring(0, i) + ',' + intString.substring(i);
+      counter = 0;
+    }
+    counter++;
+  }
+  return intString + decString;
+};
+
 export const getDollarString = (num, showZero = false) => {
   if (!num || num === 'null') {
     return showZero ? '$0' : '';
   }
 
-  const safeNum = Math.max(0, +num);
+  let safeNum = Math.max(0, +num);
 
   if (safeNum % 1 === 0) {
-    return `$${safeNum}`;
+    return `$${commafyNumber(safeNum)}`;
   }
 
-  return `$${parseFloat(safeNum).toFixed(2)}`;
+  return `$${commafyNumber(parseFloat(safeNum).toFixed(2))}`;
 };
 
 export const getMoneyLevels = (level) => {
