66865b1757acac65bc45a86e509f775a772ff8dd
Kacy Sommers <kacysommers@gmail.com>
2022-01-17 17:05:42 -0800

    add LoadingSpinner to ProtectedRoute

--- packages/sharks/src/app/ProtectedRoute.tsx
+++ packages/sharks/src/app/ProtectedRoute.tsx
@@ -1,8 +1,11 @@
-import { useAuth } from '@statrookie/core';
+import { LoadingSpinner, useAuth } from '@statrookie/core';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
 
 export const ProtectedRoute = ({ ...props }: RouteProps) => {
-  const { authorizedUser } = useAuth();
+  const { authorizedUser, initialized } = useAuth();
+  if (!initialized) {
+    return <LoadingSpinner />;
+  }
   if (!authorizedUser) {
     return <Redirect to="/dashboard" />;
   }
