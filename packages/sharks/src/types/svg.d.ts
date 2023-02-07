7e1fbf7c82cfd729196862e634d59180d29ece26
Kacy Sommers <kacysommers@gmail.com>
2023-02-07 17:07:16 -0500

    get registration page working

--- packages/sharks/src/types/svg.d.ts
+++ packages/sharks/src/types/svg.d.ts
@@ -1,1 +1,5 @@
-declare module '*.svg';
+declare module '*.svg' {
+  import { ReactElement, SVGProps } from 'react';
+  const content: (props: SVGProps<SVGElement>) => ReactElement;
+  export default content;
+}
