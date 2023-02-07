bb4ae655625d472d420926582aa7ef8d758762fa
Kacy Sommers <kacysommers@gmail.com>
2023-02-06 22:28:47 -0500

    set priority true on ice background image

--- packages/core/src/components/IceBackground/IceBackground.tsx
+++ packages/core/src/components/IceBackground/IceBackground.tsx
@@ -8,6 +8,7 @@ export const IceBackground = ({ children }: IceBackgroundProps) => {
   return (
     <div className="w-full h-full ice-background absolute bottom-0 left-0 right-0 flex items-center justify-center z-0">
       <Image
+        priority={true}
         fill={true}
         src="https://res.cloudinary.com/ddbcnnu7h/image/upload/v1675719677/field_dyjakw.png"
         alt="Hockey rink"
