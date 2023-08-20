0f3d8d8c61cb8b3e64fe1d4af0bea64da276a025
Kacy Sommers <kacysommers@gmail.com>
2023-08-20 19:21:39 -0400

    remove missing imports

--- src/app/image-loader.js
+++ src/app/image-loader.js
@@ -1,42 +1,33 @@
+import backBtnRvrsd from '@images/back-btn-reversed.svg';
 import backBtn from '@images/back-btn.svg';
-import financialsLogo from '@images/financials-logo.svg';
 import ice from '@images/field.png';
-import bluebearsLogo from '@images/icons/team-logos/bluebearsLg.svg';
-import redrabbitsLogo from '@images/icons/team-logos/redrabbitsLg.svg';
-import pinkpanthersLogo from '@images/icons/team-logos/pinkpanthersLg.svg';
-import yellowyaksLogo from '@images/icons/team-logos/yellowyaksLg.svg';
-import greengiraffesLogo from '@images/icons/team-logos/greengiraffesLg.svg';
-import goldengeckosLogo from '@images/icons/team-logos/goldengeckosLg.svg';
-import graygrasshoppersLogo from '@images/icons/team-logos/graygrasshoppersLg.svg';
-import orangeowlsLogo from '@images/icons/team-logos/orangeowlsLg.svg';
-import silverspidersLogo from '@images/icons/team-logos/silverspidersLg.svg';
-import whitewolvesLogo from '@images/icons/team-logos/whitewolvesLg.svg';
-import pinkpandasLogo from '@images/icons/team-logos/pinkpandasLg.svg';
-import blackbeaversLogo from '@images/icons/team-logos/blackbeaversLg.svg';
-import jrSharksLogo from '@images/icons/jr-sharks-logo-lg.svg';
-import sjbarracudalogo from '@images/icons/sjbarracuda-logo.svg';
-import sjsharkslogo from '@images/icons/sjsharkslogo.svg';
+import financialsLogo from '@images/financials-logo.svg';
+import iceBgSmall from '@images/ice-bg-small.svg';
+import arrowLeft from '@images/icons/arrow-left.svg';
+import arrowRight from '@images/icons/arrow-right.svg';
+import binoculars from '@images/icons/binoculars.svg';
 import cancelBtn from '@images/icons/cancel-big.svg';
 import confirmBtn from '@images/icons/confirm-big.svg';
+import jrSharksLogo from '@images/icons/jr-sharks-logo-lg.svg';
 import notepad from '@images/icons/notepaper-pen.svg';
-import arrowRight from '@images/icons/arrow-right.svg';
-import arrowLeft from '@images/icons/arrow-left.svg';
-import binoculars from '@images/icons/binoculars.svg';
-import sharkieBtn from '@images/sharkie-btn.svg';
-import iceBgSmall from '@images/ice-bg-small.svg';
-import backBtnRvrsd from '@images/back-btn-reversed.svg';
+import sjbarracudalogo from '@images/icons/sjbarracuda-logo.svg';
+import sjsharkslogo from '@images/icons/sjsharkslogo.svg';
+import abbcanucksLogo from '@images/icons/team-logos/ahl/AbbotsforCanucks.png';
+import wranglersLogo from '@images/icons/team-logos/ahl/CalgaryWranglers.png';
+import cvFirebirdsLogo from '@images/icons/team-logos/ahl/CoachellaValleyFirebirds.png';
 import condorsLogo from '@images/icons/team-logos/ahl/bakersfieldcondorsLG.png';
-import wolvesLogo from '@images/icons/team-logos/ahl/chicagowolvesLG.png';
 import eaglesLogo from '@images/icons/team-logos/ahl/coloradoeaglesLG.png';
 import silverKinghtsLogo from '@images/icons/team-logos/ahl/hendersonsilverknightsLG.png';
 import iowaWildLogo from '@images/icons/team-logos/ahl/iowawildLG.png';
-import mooseLogo from '@images/icons/team-logos/ahl/manitobamooseLG.png';
 import reignLogo from '@images/icons/team-logos/ahl/ontarioreignLG.png';
-import iceHogsLogo from '@images/icons/team-logos/ahl/rockfordicehogsLG.png';
 import gullesLogo from '@images/icons/team-logos/ahl/sandiegogullsLG.png';
-import heatLogo from '@images/icons/team-logos/ahl/stocktonheatLG.png';
 import texasStartsLogo from '@images/icons/team-logos/ahl/texasstarsLG.png';
 import roadRunnersLogo from '@images/icons/team-logos/ahl/tucsonroadrunnersLG.png';
+import blackbeaversLogo from '@images/icons/team-logos/blackbeaversLg.svg';
+import bluebearsLogo from '@images/icons/team-logos/bluebearsLg.svg';
+import goldengeckosLogo from '@images/icons/team-logos/goldengeckosLg.svg';
+import graygrasshoppersLogo from '@images/icons/team-logos/graygrasshoppersLg.svg';
+import greengiraffesLogo from '@images/icons/team-logos/greengiraffesLg.svg';
 import ducksLogo from '@images/icons/team-logos/nhl/anaheimducksLG.png';
 import coyotesLogo from '@images/icons/team-logos/nhl/arizonacoyotesLG.png';
 import flamesLogo from '@images/icons/team-logos/nhl/calgaryflamesLG.png';
@@ -49,6 +40,14 @@ import bluesLogo from '@images/icons/team-logos/nhl/stlouisbluesLG.png';
 import canucksLogo from '@images/icons/team-logos/nhl/vancouvercanucksLG.png';
 import goldenKnightsLogo from '@images/icons/team-logos/nhl/vegasgoldenknightsLG.png';
 import jetsLogo from '@images/icons/team-logos/nhl/winnipegjetsLG.png';
+import orangeowlsLogo from '@images/icons/team-logos/orangeowlsLg.svg';
+import pinkpandasLogo from '@images/icons/team-logos/pinkpandasLg.svg';
+import pinkpanthersLogo from '@images/icons/team-logos/pinkpanthersLg.svg';
+import redrabbitsLogo from '@images/icons/team-logos/redrabbitsLg.svg';
+import silverspidersLogo from '@images/icons/team-logos/silverspidersLg.svg';
+import whitewolvesLogo from '@images/icons/team-logos/whitewolvesLg.svg';
+import yellowyaksLogo from '@images/icons/team-logos/yellowyaksLg.svg';
+import sharkieBtn from '@images/sharkie-btn.svg';
 import { getTeamVideos } from './data/season/season';
 
 const images = [
@@ -80,15 +79,14 @@ const images = [
   binoculars,
   sharkieBtn,
   condorsLogo,
-  wolvesLogo,
+  cvFirebirdsLogo,
   eaglesLogo,
   silverKinghtsLogo,
   iowaWildLogo,
-  mooseLogo,
+  wranglersLogo,
   reignLogo,
-  iceHogsLogo,
+  abbcanucksLogo,
   gullesLogo,
-  heatLogo,
   texasStartsLogo,
   roadRunnersLogo,
   ducksLogo,
